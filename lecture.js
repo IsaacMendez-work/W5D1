e995133fadb274b951c30cc19739ea331c91cce4
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-15 14:25:40 -0400

    commit

--- lecture.js
+++ lecture.js
@@ -1,10 +1,28 @@
-// Chaining promises together 
-const aRandomPromise =
-  (new Promise(myExecutorFunc))
-  .then(handleFulfilledA)
-  .then(handleFulfilledB)
-  .then(handleFulfilledC)
-  .catch(handleRejectedAny);
+// // Chaining promises together 
+// const aRandomPromise =
+//   (new Promise(myExecutorFunc))
+//   .then(handleFulfilledA)
+//   .then(handleFulfilledB)
+//   .then(handleFulfilledC)
+//   .catch(handleRejectedAny);
 
-const somePromise = (new Promise(promiseArgument)).then(fulfillment1).then(fulfillment2).fulfillment2).catch(rejectionMessage);
+// // Exercise one is to turn this...
+// function loadJson(url) {
+//   return fetch(url)
+//     .then(response => {
+//       if (response.status == 200) {
+//         return response.json();
+//       } else {
+//         throw new Error(response.status);
+//       }
+//     });
+// }
+//
+// loadJson('http://httpstat.us/500')
+//   .catch(alert); // Error: 500
 
+// // ... into a cleaner async/await function
+ async function jsonURL() {
+     let response = await fetch(url)
+     
+ }
